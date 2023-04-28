54a1b9f8611463e3b0309a1e32748b816099061d
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-27 20:16:13 -0500

    feat: Add a simple route that returns a file
    This route just responds with the contents of the tags file. The file
    will be parsed on the client side because I can't be bothered.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use serde::Serialize;
 use std::fs;
 
+#[derive(Serialize)]
+struct Message {
+    message: String,
+}
+
+#[get("/api/tags")]
+async fn tags() -> impl Responder {
+    let contents = fs::read_to_string("content/tags").expect("Something went wrong while reading the file");
+    HttpResponse::Ok().json(Message {
+        message: contents
+    })
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let bind_address = "127.0.0.1:8080";
@@ -17,6 +30,7 @@ async fn main() -> std::io::Result<()> {
 
         App::new()
             .wrap(cors)
+            .service(tags)
     })
     .bind(bind_address)?
     .run()
