8ef72ec2fa6949c783e33bce19dcf16e6104506d
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-30 18:50:27 -0500

    style: Cleaned up formatting
    Applied a formatter and fixed some stylistic issues with the code.
    
    Turns out code is more readable when it is not all on one line.

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,22 @@ struct Message {
 #[get("/api/tags")]
 async fn tags() -> impl Responder {
     let mut contents = String::new();
-    for entry in fs::read_dir("content/posts/published").expect("Something went wrong while reading the directory") {
+    for entry in fs::read_dir("content/posts/published")
+        .expect("Something went wrong while reading the directory")
+    {
         let entry = entry.expect("Something went wrong while reading the entry");
         let path = entry.path();
         let file = fs::read_to_string(path).expect("Something went wrong while reading the file");
-        let lines: Vec<&str> = file.split("\n").collect();
+        let lines: Vec<&str> = file.split('\n').collect();
 
-        contents+=lines[2].split(":").last().unwrap().trim().replace(", ", "\n").as_str();
-        contents+="\n";
+        contents += lines[2]
+            .split(':')
+            .last()
+            .expect("String formatted incorrectly")
+            .trim()
+            .replace(", ", "\n")
+            .as_str();
+        contents += "\n";
     }
     HttpResponse::Ok().json(Message { message: contents })
 }
@@ -38,7 +46,7 @@ async fn posts(id: web::Path<String>) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     let bind_address = "127.0.0.1:8080";
 
-    println!("Starting server at {}", bind_address);
+    println!("Starting server at {bind_address}");
 
     HttpServer::new(|| {
         let cors = Cors::default()
